097099f315494f4b79ec3a259c292012f9478117
Brian Ho <bh1525@nyu.edu>
2024-04-04 17:16:30 -0400

    first commit

---
+++ .history/main_20240404155744.js
@@ -0,0 +1,92 @@
+import './style.css'
+import * as THREE from 'three'
+import {
+	addBoilerPlateMesh,
+	addStandardMesh,
+	addPlanet1,
+	addPlanet2,
+} from './addMeshes'
+import { addLight } from './addLights'
+import Model from './Model'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
+
+const scene = new THREE.Scene()
+const renderer = new THREE.WebGLRenderer({ antialias: true })
+const camera = new THREE.PerspectiveCamera(
+	75,
+	window.innerWidth / window.innerHeight,
+	0.1,
+	100
+)
+camera.position.set(0, 0, 5)
+
+const meshes = {}
+const mixers = []
+let tick = 0
+const controls = new OrbitControls(camera, renderer.domElement)
+init()
+function init() {
+	renderer.setSize(window.innerWidth, window.innerHeight)
+	document.body.appendChild(renderer.domElement)
+
+	//meshes
+	meshes.default = addBoilerPlateMesh()
+	meshes.standard = addStandardMesh()
+	meshes.planet1 = addPlanet1()
+	meshes.planet2 = addPlanet2()
+
+	//lights
+	meshes.defaultLight = addLight()
+
+	//changes
+	meshes.default.scale.set(2, 2, 2)
+
+	//scene operations
+	scene.add(meshes.default)
+	scene.add(meshes.standard)
+	scene.add(meshes.defaultLight)
+
+	models()
+	resize()
+	animate()
+}
+
+function models() {
+	const Flower = new Model({
+		url: '/flowers.glb',
+		meshes: meshes,
+		name: 'flower',
+		scene: scene,
+		animationState: true,
+		mixers: mixers,
+		replace: true,
+	})
+	Flower.init()
+}
+
+function resize() {
+	window.addEventListener('resize', () => {
+		renderer.setSize(window.innerWidth, window.innerHeight)
+		camera.aspect = window.innerWidth / window.innerHeight
+		camera.updateProjectionMatrix()
+	})
+}
+
+function animate() {
+	requestAnimationFrame(animate)
+	tick += 0.1
+
+	meshes.default.rotation.x += 0.01
+	meshes.default.rotation.z += 0.01
+
+	meshes.standard.rotation.x += 0.01
+	meshes.standard.rotation.z += 0.01
+
+	for (const mixer in mixers) {
+		mixer.update(tick)
+	}
+
+	// meshes.default.scale.x += 0.01
+
+	renderer.render(scene, camera)
+}
